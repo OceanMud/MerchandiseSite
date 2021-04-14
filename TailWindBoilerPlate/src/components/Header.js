912c92a42451890b1d693f1585f6dfbd11546e8f
OceanMud <oceanmudhs@gmail.com>
2021-04-13 20:40:01 -0700

    side menu

--- TailWindBoilerPlate/src/components/Header.js
+++ TailWindBoilerPlate/src/components/Header.js
@@ -5,6 +5,9 @@ const Header = () => {
   const [menuActive, setMenuActive] = useState(false);
   const [subMenu, setSubMenu] = useState(false);
   const [visible, setVisible] = useState("absolute");
+  const [sideMenu, setSideMenu] = useState(false);
+  const [transition, setTransition] = useState("-translate-x-96 ");
+
   const subRef = useRef(subMenu);
   subRef.current = subMenu;
 
@@ -24,15 +27,114 @@ const Header = () => {
 
   return (
     <>
-      <div className="border-gray-200 border-b      ">
+      <div className="border-gray-200 border-b  cursor-pointer     ">
+        <div className="block md:hidden ">
+          {sideMenu && (
+            <div className=" fixed   z-30">
+              <div
+                className={` bg-white h-screen w-96 ${transition} transition transform ease-in-out duration-300 `}
+              >
+                <div className="border-t border-b border-gray-200">
+                  <p
+                    className=" text-xs p-2 "
+                    onClick={() => {
+                      setTransition("-translate-x-96");
+                      setTimeout(() => {
+                        setSideMenu(false);
+                        document.body.style.overflow = "visible";
+                      }, 400);
+                    }}
+                  >
+                    <img
+                      className=" filter contrast-0   object-center h-6 "
+                      src="/images/close.svg"
+                    />
+                    close
+                  </p>
+                </div>
+                <div className=" text-sm p-2 flex flex-col space-y-6 text-gray-700  ">
+                  <a href="#" className=" hover:text-gray-900">
+                    Home
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    Products
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    Services
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    Integrations
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    How it works
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    Mockup Generator
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    Resources
+                  </a>
+                  <a href="#" className="hover:text-gray-900">
+                    Blog
+                  </a>
+                </div>
+                <div className="flex  items-center px-2  flex-col">
+                  <a
+                    href="#"
+                    className="bg-red-600 w-full hover:shadow-lg hover:border-black border
+                  
+                     text-white  font-semibold text-sm text-center py-2 rounded-md mt-2 mb-2  "
+                  >
+                    Sign Up
+                  </a>
+                  <a
+                    href="#"
+                    className="bg-gray-300 w-full border hover:shadow-md hover:border-black  font-semibold text-sm text-center text-gray-900 py-2 rounded-md mb-2 "
+                  >
+                    Sign In
+                  </a>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {sideMenu && (
+            <div
+              className="inset-0 fixed z-20  "
+              onClick={() => {
+                setTransition("-translate-x-96");
+                setTimeout(() => {
+                  setSideMenu(false);
+                  document.body.style.overflow = "visible";
+                }, 400);
+              }}
+            >
+              <div className="bg-gray-900  bg-opacity-50 h-screen w-screen"></div>
+            </div>
+          )}
+        </div>
+
         <div className="flex max-w-screen-2xl mx-auto py-4 md:px-6  justify-between  pr-5 bottom-1 text-gray-500">
           <div className="flex pl-2  md:w-auto w-full  ">
-            <div>
+            <div
+              onClick={() => {
+                if (!sideMenu) {
+                  setTimeout(() => {
+                    setTransition("translate-x-0");
+                  }, 100);
+                  document.body.style.overflow = "hidden";
+                  setSideMenu(true);
+                }
+              }}
+              className="cursor-pointer group"
+            >
               <img
-                className=" md:hidden block h-6 filter contrast-0  "
+                className=" md:hidden block h-6 filter contrast-0 group-hover:contrast-75  "
                 src="/images/menu.svg"
               />
-              <p className=" md:hidden text-xs ">menu</p>
+              <p className=" md:hidden text-xs group-hover:text-gray-900  ">
+                menu
+              </p>
             </div>
 
             <div className="flex w-full items-center justify-center  ">
